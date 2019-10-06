1f30b66fcb63de23e294566d68938c4d6a58c3ba
Steven Meiklejohn <stevenmeiklejohn700@gmail.com>
2019-10-06 15:37:39 +0100

    Article component rendering with button to go to next article.

--- src/containers/ArticlesContainer.js
+++ src/containers/ArticlesContainer.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import Article1 from './../components/Article1.js';
-
-
-
 
 class ArticlesContainer extends React.Component {
   constructor(props) {
@@ -10,54 +6,32 @@ class ArticlesContainer extends React.Component {
 
     this.state = {
       currentArticle: 0,
-      articleElements: [],
-
+      formattedArticles: []
     }
 
-    this.createArticleElements = this.createArticleElements.bind(this);
-
+    this.switchArticle = this.switchArticle.bind(this);
   }
 
-  createArticleElements(){
-    let allArticles = []
-    let singleArticleElements = [];
-    console.log('incoming articles data', this.props.articles.articles);
-    for(let data of this.props.articles.articles){
-      console.log('data', data);
-      for(let element of data.body){
-        console.log('element', element);
-      if(element.type === "heading"){
-        singleArticleElements.push(<h1>Heading: {element.model.text}</h1>);
-      } else if (element.type === "paragraph") {
-        singleArticleElements.push(<p> Paragraph: {element.model.text}</p>)
-      } else if (element.type === "image") {
-        console.log('image insert triggered');
-        singleArticleElements.push(<img id="articleImage" src={element.model.url}></img>)
-      }
-      console.log('singleArticleElements', singleArticleElements);
+  switchArticle(){
+    if(this.state.currentArticle === 4){
+      this.setState((prevState) => ({currentArticle: 0}))
+    } else {
+      this.setState((prevState) => ({currentArticle: prevState.currentArticle +1}))
     }
-      console.log('singleArticleElements post loop', singleArticleElements);
-      allArticles.push(singleArticleElements);
-      singleArticleElements = [];
-    }
-    console.log('allArticles', allArticles);
-    return allArticles;
   }
-
-
-
-
-
 
   render() {
+    console.log('current article', this.state.currentArticle);
+    console.log('formattedArticles prop', this.props.articles);
+
     if (this.props.articles == null || this.props.articles.length === 0) {
   return <p>Loading...</p>;
   }
-  let elements = this.createArticleElements();
-  console.log(this.props.articles.articles[0].body[0].model.text);
-    return (
+
+  return (
       <div>
-        {elements}
+        {this.props.articles[this.state.currentArticle]}
+        <button className ="nextArticleButton" onClick={this.switchArticle}>Next</button>
       </div>
     )
   }
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -9,15 +9,44 @@ class MainContainer extends React.Component {
     super(props);
 
     this.state = {
-      stories: [],
+      recievedData: [],
+      formattedArticles: []
     }
+    this.formatArticleData = this.formatArticleData.bind(this);
+    this.fetchData();
   }
 
-  componentDidMount() {
+  fetchData(){
     APIRequest("articlesApi")
     .then((results) => {
-      this.setState({ stories: results })
+      this.setState({ recievedData: results }, this.formatArticleData)
     })
+  }
+
+  formatArticleData(){
+    let allArticles = []
+    let singleArticleElements = [];
+    console.log('incoming articles data', this.state.recievedData.articles);
+    for(let data of this.state.recievedData.articles){
+      console.log('data', data);
+      for(let element of data.body){
+        console.log('element', element);
+      if(element.type === "heading"){
+        singleArticleElements.push(<h1>Heading: {element.model.text}</h1>);
+      } else if (element.type === "paragraph") {
+        singleArticleElements.push(<p> Paragraph: {element.model.text}</p>)
+      } else if (element.type === "image") {
+        console.log('image insert triggered');
+        singleArticleElements.push(<img id="articleImage" src={element.model.url}></img>)
+      }
+      console.log('singleArticleElements', singleArticleElements);
+    }
+      console.log('singleArticleElements post loop', singleArticleElements);
+      allArticles.push(singleArticleElements);
+      singleArticleElements = [];
+    }
+    console.log('allArticles', allArticles);
+    this.setState({formattedArticles: allArticles});
   }
 
 
@@ -27,7 +56,7 @@ class MainContainer extends React.Component {
     return (
       <div>
       <ArticlesContainer
-        articles={this.state.stories}
+        articles={this.state.formattedArticles}
         />
       </div>
     )
